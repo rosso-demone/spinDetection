74527049f04b021e0b4c81d9a178fc9e07282122
rosso-demone <chaitanya.k.patel@hotmail.com>
2018-07-23 22:04:00 +0530

    Project files added

---
+++ SpinDetection/SpinDetect.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SpinDetection
+{
+    partial class SpinDetect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SpinDetect));
+            this.cameraViewBox = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.cameraViewBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cameraViewBox
+            // 
+            this.cameraViewBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cameraViewBox.Location = new System.Drawing.Point(12, 12);
+            this.cameraViewBox.Name = "cameraViewBox";
+            this.cameraViewBox.Size = new System.Drawing.Size(1068, 600);
+            this.cameraViewBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cameraViewBox.TabIndex = 2;
+            this.cameraViewBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 618);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(327, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Load Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1005, 618);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SpinDetect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 657);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cameraViewBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SpinDetect";
+            this.Text = "Spin Detector";
+            ((System.ComponentModel.ISupportInitialize)(this.cameraViewBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox cameraViewBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ SpinDetection/SpinDetect.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.Data.SqlClient;
+using System.IO;
+using System.Configuration;
+
+namespace SpinDetection
+{
+    public partial class SpinDetect : Form
+    {
+        Image<Gray, byte> GrayFrame;
+        Image<Bgr, byte> Frames;
+        public CascadeClassifier spin = new CascadeClassifier("SPIN_HOG.xml");
+
+        public SpinDetect()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessFrame()
+        {
+            GrayFrame = Frames.Convert<Gray, byte>();
+
+            Rectangle[] spinsDetected = spin.DetectMultiScale(GrayFrame, 1.1, 3, new Size(20, 20), Size.Empty);
+
+            for (int i = 0; i < spinsDetected.Length; i++)
+            {
+                Frames.Draw(spinsDetected[i], new Bgr(Color.Red), 2);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cameraViewBox.ClearOperation();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Image InputImg = Image.FromFile(openFileDialog.FileName);
+                Frames = new Image<Bgr, byte>(new Bitmap(InputImg));
+                cameraViewBox.Image = Frames;
+            }
+            ProcessFrame();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
